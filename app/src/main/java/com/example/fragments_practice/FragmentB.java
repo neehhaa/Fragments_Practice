f684089261ad8da2f9e0e67df490a0bfa67d2b8b
neha_wasnik <neha.wasnik@ftcash.com>
2023-02-27 01:03:41 +0530

    static

---
+++ app/src/main/java/com/example/fragments_practice/FragmentB.java
@@ -0,0 +1,26 @@
+package com.example.fragments_practice;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class FragmentB extends Fragment {
+
+    public FragmentB() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_b, container, false);
+        TextView textView = view.findViewById(R.id.txt_FragB);
+        return view;
+    }
+}
--- app/src/main/java/com/example/fragments_practice/MainActivity.java
+++ app/src/main/java/com/example/fragments_practice/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.fragments_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnFragA,btnFragB,btnFragC;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFragA = findViewById(R.id.btnFragA);
+        btnFragB = findViewById(R.id.btnFragB);
+        btnFragC = findViewById(R.id.btnFragC);
+
+        btnFragA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+
+            }
+        });
+
+        btnFragB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnFragC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
